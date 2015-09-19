12f3993e54cfd1007c5cfc343d72b5b2b121bc92
Ben Drucker <bvdrucker@gmail.com>
2015-09-19 16:10:10 -0700

    Add runtime assertions

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 'use strict'
 
+var assertObserv = require('assert-observ')
+var assert = require('assert-ok')
+
 module.exports = function observIncrement (observable, increment) {
+  assertObserv(observable)
   if (increment == null) increment = 1
+  assert(typeof increment === 'number', 'expected number increment')
+
   observable.set(observable() + increment)
 }
